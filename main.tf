f9e5fb1082cc1ae5dc653b583fa86c2eaf745656
Bohachov Maksym <70446041+rednikeeg@users.noreply.github.com>
2025-07-07 23:31:32 +0300

    update pipeline + terraform

--- main.tf
+++ main.tf
@@ -17,4 +17,9 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "cicd-shop-rg"
   location = "norwayeast"
+
+  tags = {
+    Environment = "Azure + Terraform + Github Actions"
+    Team        = "DevOps"
+  }
 }
