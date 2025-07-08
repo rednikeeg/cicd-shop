a77288ba9c3aec7917b44cc335610ebea8c35cdf
Bohachov, Maksym <m.bohachov@consulting.mk>
2025-07-08 14:59:44 +0300

    add destroy infra pipeline

--- manage_state.tf
+++ manage_state.tf
@@ -1,12 +1,8 @@
-terraform {
-  backend "azurerm" {
-      resource_group_name  = "tfstate"
-      storage_account_name = "tfstate_main"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
+# terraform {
+#   backend "azurerm" {
+#       resource_group_name  = "tfstate"
+#       storage_account_name = "tfstate_main"
+#       container_name       = "tfstate"
+#       key                  = "terraform.tfstate"
+#   }
+# }
