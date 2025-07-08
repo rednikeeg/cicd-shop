36ff0f781a494856a355a86461a78f2202c4daac
Bohachov, Maksym <m.bohachov@consulting.mk>
2025-07-08 15:18:18 +0300

    upd terraform

--- manage_state.tf
+++ manage_state.tf
@@ -1,7 +1,7 @@
 # terraform {
 #   backend "azurerm" {
 #       resource_group_name  = "tfstate"
-#       storage_account_name = "tfstate_main"
+#       storage_account_name = "tfstatemain"
 #       container_name       = "tfstate"
 #       key                  = "terraform.tfstate"
 #   }
